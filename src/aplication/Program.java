461fe32da7379733d017325387471d674856aee3
ivanfigueiredo <ivan.figueiredo99@gmail.com>
2021-01-24 13:29:29 -0300

    Adicionei estrutura condicional IF/ELSE para teste de idade

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,25 @@
 package aplication;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String nome;
+		int idade;
 		
-		String nome = "Ivan";
-		int idade = 34;
+		idade = input.nextInt();
+		input.nextLine();
+		nome = input.nextLine();
 		
-		System.out.println(nome);
-		System.out.println(idade);
+		if (idade > 18) {
+			System.out.printf("%s tem %d. Portanto, maior de 18 nos.",nome, idade);
+		}else {
+			System.out.printf("%s tem %d. Portanto, menor de 18 anos.",nome, idade);
+		}
+		
+		input.close();
 
 	}
 
